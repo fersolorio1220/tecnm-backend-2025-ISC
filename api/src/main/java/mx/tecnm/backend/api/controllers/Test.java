75d0938dc7440d6869cac665a6fb4974dcb4a886
estrellaGonzalez02 <estrellagonzalez2502@gmail.com>
2025-11-26 17:09:05 +0000

    actualizacion

--- api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -1,6 +1,8 @@
 package mx.tecnm.backend.api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,16 +34,47 @@ public class Test {
         p1.codigoBarras = "1234567890987";
 
         Producto p2 = new Producto();
-        p2.nombre = "coca cola";
-        p2.precio = 18.5;
-        p2.codigoBarras = "1234567890987";
+        p2.nombre = "Pepsi";
+        p2.precio = 17.5;
+        p2.codigoBarras = "0973567890987";
         
         Producto p3 = new Producto();
-        p3.nombre = "coca cola";
-        p3.precio = 18.5;
-        p3.codigoBarras = "1234567890987";
+        p3.nombre = "Fanta";
+        p3.precio = 16.5;
+        p3.codigoBarras = "0283764246197";
         
         return new Producto[]{p1, p2, p3};
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> buscarPorId(@PathVariable int id) {
+
+        if (id<0||id >2) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Producto[] productos = new Producto[3];
+        Producto p1 = new Producto();
+        p1.nombre = "coca cola";
+        p1.precio = 18.5;
+        p1.codigoBarras = "1234567890987";
+        productos[0] = p1;
+
+
+        Producto p2 = new Producto();
+        p2.nombre = "pepsi";
+        p2.precio = 17.5;
+        p2.codigoBarras = "0234567890987";
+        productos[1] = p2;
+        
+        Producto p3 = new Producto();
+        p3.nombre = "Fanta";
+        p3.precio = 16.5;
+        p3.codigoBarras = "47208371271237";
+        productos[2] = p3;
+        
+        Producto resultado = productos[id];
+        return ResponseEntity.ok(resultado);
     }
 
     
---
+++ api/src/main/java/mx/tecnm/backend/api/repository/CategoriaDAO.java
@@ -0,0 +1,27 @@
+package mx.tecnm.backend.api.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import mx.tecnm.backend.api.models.Categoria;
+
+    
+@Repository
+public class CategoriaDAO {
+
+    @Autowired
+    private JdbcClient conexion;
+
+    public List<Categoria> conusultarCategorias() {
+        String sql = "SELECT id, nombre FROM categorias";
+        return conexion.sql(sql)
+                .query((rs, rowNum) -> new Categoria(
+                    rs.getInt("id"),
+                    rs.getString("nombre")))
+                    .list();
+    }
+
+}
